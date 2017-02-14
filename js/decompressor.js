366b837eca8a8b4bc37c019301959ac1a42c80b2
David Bau <david.bau@gmail.com>
2017-02-14 07:44:52 -0500

    Fixed decompressor bug.

--- js/decompressor.js
+++ js/decompressor.js
@@ -14,7 +14,7 @@
 (function(input) {
   var f = String.fromCharCode,
       doc = document,
-      /*length = input.length, */
+      length = input.length,
       resetValue = 32,
       keyStrBase64 =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
@@ -33,27 +33,28 @@
         w,
         bits, resb, maxpower, power,
         c,
-        data = {v:getNextValue(0), p:resetValue, i:1};
+        data;
 
     for (i = 0; i < keyStrBase64.length ; i++) {
       baseReverseDic[keyStrBase64.charAt(i)] = i;
     }
 
     function getNextValue(i) { return baseReverseDic[input.charAt(i)]; }
+    data = {val:getNextValue(0), position:resetValue, index:1};
 
     for (i = 0; i < 3; i += 1) {
       dictionary[i] = i;
     }
 
     bits = 0;
-    maxpower = mpow(2,2);
+    maxpower = Math.pow(2,2);
     power=1;
     while (power!=maxpower) {
-      resb = data.v & data.p;
-      data.p >>= 1;
-      if (data.p == 0) {
-        data.p = resetValue;
-        data.v = getNextValue(data.i++);
+      resb = data.val & data.position;
+      data.position >>= 1;
+      if (data.position == 0) {
+        data.position = resetValue;
+        data.val = getNextValue(data.index++);
       }
       bits |= (resb>0 ? 1 : 0) * power;
       power <<= 1;
@@ -62,14 +63,14 @@
     switch (next = bits) {
       case 0:
           bits = 0;
-          maxpower = mpow(2,8);
+          maxpower = Math.pow(2,8);
           power=1;
           while (power!=maxpower) {
-            resb = data.v & data.p;
-            data.p >>= 1;
-            if (data.p == 0) {
-              data.p = resetValue;
-              data.v = getNextValue(data.i++);
+            resb = data.val & data.position;
+            data.position >>= 1;
+            if (data.position == 0) {
+              data.position = resetValue;
+              data.val = getNextValue(data.index++);
             }
             bits |= (resb>0 ? 1 : 0) * power;
             power <<= 1;
@@ -78,44 +79,40 @@
         break;
       case 1:
           bits = 0;
-          maxpower = mpow(2,16);
+          maxpower = Math.pow(2,16);
           power=1;
           while (power!=maxpower) {
-            resb = data.v & data.p;
-            data.p >>= 1;
-            if (data.p == 0) {
-              data.p = resetValue;
-              data.v = getNextValue(data.i++);
+            resb = data.val & data.position;
+            data.position >>= 1;
+            if (data.position == 0) {
+              data.position = resetValue;
+              data.val = getNextValue(data.index++);
             }
             bits |= (resb>0 ? 1 : 0) * power;
             power <<= 1;
           }
         c = f(bits);
         break;
-      /* Should never happen
       case 2:
-        return "";
-      */
+        return "hm,A";
     }
     dictionary[3] = c;
     w = c;
     result.push(c);
     while (true) {
-      /* Should never happen
-      if (data.i > length) {
-        return "";
+      if (data.index > length) {
+        return "hm,B";
       }
-     */
 
       bits = 0;
-      maxpower = mpow(2,numBits);
+      maxpower = Math.pow(2,numBits);
       power=1;
       while (power!=maxpower) {
-        resb = data.v & data.p;
-        data.p >>= 1;
-        if (data.p == 0) {
-          data.p = resetValue;
-          data.v = getNextValue(data.i++);
+        resb = data.val & data.position;
+        data.position >>= 1;
+        if (data.position == 0) {
+          data.position = resetValue;
+          data.val = getNextValue(data.index++);
         }
         bits |= (resb>0 ? 1 : 0) * power;
         power <<= 1;
@@ -124,14 +121,14 @@
       switch (c = bits) {
         case 0:
           bits = 0;
-          maxpower = mpow(2,8);
+          maxpower = Math.pow(2,8);
           power=1;
           while (power!=maxpower) {
-            resb = data.v & data.p;
-            data.p >>= 1;
-            if (data.p == 0) {
-              data.p = resetValue;
-              data.v = getNextValue(data.i++);
+            resb = data.val & data.position;
+            data.position >>= 1;
+            if (data.position == 0) {
+              data.position = resetValue;
+              data.val = getNextValue(data.index++);
             }
             bits |= (resb>0 ? 1 : 0) * power;
             power <<= 1;
@@ -143,14 +140,14 @@
           break;
         case 1:
           bits = 0;
-          maxpower = mpow(2,16);
+          maxpower = Math.pow(2,16);
           power=1;
           while (power!=maxpower) {
-            resb = data.v & data.p;
-            data.p >>= 1;
-            if (data.p == 0) {
-              data.p = resetValue;
-              data.v = getNextValue(data.i++);
+            resb = data.val & data.position;
+            data.position >>= 1;
+            if (data.position == 0) {
+              data.position = resetValue;
+              data.val = getNextValue(data.index++);
             }
             bits |= (resb>0 ? 1 : 0) * power;
             power <<= 1;
@@ -160,11 +157,11 @@
           enlargeIn--;
           break;
         case 2:
-          return doc.write(result.join(''));
+          return result.join('');
       }
 
       if (enlargeIn == 0) {
-        enlargeIn = mpow(2, numBits);
+        enlargeIn = Math.pow(2, numBits);
         numBits++;
       }
 
@@ -173,12 +170,9 @@
       } else {
         if (c === dictSize) {
           entry = w + w.charAt(0);
-        }
-       /* Should never happen
-        else {
+        } else {
           return null;
         }
-       */
       }
       result.push(entry);
 
@@ -189,7 +183,7 @@
       w = entry;
 
       if (enlargeIn == 0) {
-        enlargeIn = mpow(2, numBits);
+        enlargeIn = Math.pow(2, numBits);
         numBits++;
       }
 
