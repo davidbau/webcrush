008db5da5b683bcda30ce9fd71c9107a0089ea19
David Bau <david.bau@gmail.com>
2017-02-14 02:01:27 -0500

    Update to include web ui.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Uglify decompressor, fold it to 70 chars per line, format it as a string,
+# and insert it into webcrush.js.
+node_modules/uglifyjs/bin/uglifyjs js/decompressor.js -c -m | \
+  sed 's/("");$//' | \
+  fold -w 70 | \
+  sed "s/^/    '/" | sed "$ ! s/$/' +/" | sed "$ s/$/' \/* end *\/\n/" | \
+  sed -e "/'!function(/r /dev/stdin" \
+      -e "/'!function(/,/}' \/\* end \*\//d" \
+      -i lib/webcrush.js
+
+# Browserify webcrush to create an HTML version of the utility.
+node_modules/browserify/bin/cmd.js lib/webcrush.js -o js/webcrush.js
--- js/decompressor.js
+++ js/decompressor.js
@@ -1,7 +1,15 @@
+// Decompressor derived from LZ-String, with unused code removed.
+// Portions licensed under WTFPL
+// Copyright (c) 2013 Pieroxy <pieroxy@pieroxy.net>
+// This work is free. You can redistribute it and/or modify it
+// under the terms of the WTFPL, Version 2
+// For more information see LICENSE.txt or http://www.wtfpl.net/
+//
+// For more information, the home page:
+// http://pieroxy.net/blog/pages/lz-string/testing.html
+//
+// LZ-based compression algorithm, version 1.4.4
 /*
-Decompressor derived from LZ-String, with unused code removed.
-Copyright (c) 2013 Pieroxy <pieroxy@pieroxy.net>
-Licensed under the WTFPL license.
 */
 (function(input) {
   var f = String.fromCharCode,
@@ -11,182 +19,180 @@ Licensed under the WTFPL license.
       keyStrBase64 =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
       baseReverseDic = {},
+      mpow = Math.pow,
+      dictionary = [],
 
   // Original decompress starts here.
-	    dictionary = [],
-			next,
-			enlargeIn = 4,
-			dictSize = 4,
-			numBits = 3,
-			entry = "",
-			result = [],
-      mpow = Math.pow,
-			i,
-			w,
-			bits, resb, maxpower, power,
-			c,
-			data = {v:getNextValue(0), p:resetValue, i:1};
+        next,
+        enlargeIn = 4,
+        dictSize = 4,
+        numBits = 3,
+        entry = "",
+        result = [],
+        i,
+        w,
+        bits, resb, maxpower, power,
+        c,
+        data = {v:getNextValue(0), p:resetValue, i:1};
 
-  for (i = 0; i < keyStrBase64.length ; i++) {
-    baseReverseDic[keyStrBase64.charAt(i)] = i;
-  }
-  function getNextValue(i) { return baseReverseDic[input.charAt(i)]; }
+    for (i = 0; i < keyStrBase64.length ; i++) {
+      baseReverseDic[keyStrBase64.charAt(i)] = i;
+    }
 
-  // doc.scripts[doc.scripts.length - 1].remove();
+    function getNextValue(i) { return baseReverseDic[input.charAt(i)]; }
 
+    for (i = 0; i < 3; i += 1) {
+      dictionary[i] = i;
+    }
 
-	for (i = 0; i < 3; i += 1) {
-		dictionary[i] = i;
-	}
+    bits = 0;
+    maxpower = mpow(2,2);
+    power=1;
+    while (power!=maxpower) {
+      resb = data.v & data.p;
+      data.p >>= 1;
+      if (data.p == 0) {
+        data.p = resetValue;
+        data.v = getNextValue(data.i++);
+      }
+      bits |= (resb>0 ? 1 : 0) * power;
+      power <<= 1;
+    }
 
-	bits = 0;
-	maxpower = mpow(2,2);
-	power=1;
-	while (power!=maxpower) {
-		resb = data.v & data.p;
-		data.p >>= 1;
-		if (data.p == 0) {
-			data.p = resetValue;
-			data.v = getNextValue(data.i++);
-		}
-		bits |= (resb>0 ? 1 : 0) * power;
-		power <<= 1;
-	}
+    switch (next = bits) {
+      case 0:
+          bits = 0;
+          maxpower = mpow(2,8);
+          power=1;
+          while (power!=maxpower) {
+            resb = data.v & data.p;
+            data.p >>= 1;
+            if (data.p == 0) {
+              data.p = resetValue;
+              data.v = getNextValue(data.i++);
+            }
+            bits |= (resb>0 ? 1 : 0) * power;
+            power <<= 1;
+          }
+        c = f(bits);
+        break;
+      case 1:
+          bits = 0;
+          maxpower = mpow(2,16);
+          power=1;
+          while (power!=maxpower) {
+            resb = data.v & data.p;
+            data.p >>= 1;
+            if (data.p == 0) {
+              data.p = resetValue;
+              data.v = getNextValue(data.i++);
+            }
+            bits |= (resb>0 ? 1 : 0) * power;
+            power <<= 1;
+          }
+        c = f(bits);
+        break;
+      /* Should never happen
+      case 2:
+        return "";
+      */
+    }
+    dictionary[3] = c;
+    w = c;
+    result.push(c);
+    while (true) {
+      /* Should never happen
+      if (data.i > length) {
+        return "";
+      }
+     */
 
-	switch (next = bits) {
-		case 0:
-				bits = 0;
-				maxpower = mpow(2,8);
-				power=1;
-				while (power!=maxpower) {
-					resb = data.v & data.p;
-					data.p >>= 1;
-					if (data.p == 0) {
-						data.p = resetValue;
-						data.v = getNextValue(data.i++);
-					}
-					bits |= (resb>0 ? 1 : 0) * power;
-					power <<= 1;
-				}
-			c = f(bits);
-			break;
-		case 1:
-				bits = 0;
-				maxpower = mpow(2,16);
-				power=1;
-				while (power!=maxpower) {
-					resb = data.v & data.p;
-					data.p >>= 1;
-					if (data.p == 0) {
-						data.p = resetValue;
-						data.v = getNextValue(data.i++);
-					}
-					bits |= (resb>0 ? 1 : 0) * power;
-					power <<= 1;
-				}
-			c = f(bits);
-			break;
-    /* Should not happen
-		case 2:
-			return "";
-    */
-	}
-	dictionary[3] = c;
-	w = c;
-	result.push(c);
-	while (true) {
-    /* Should not happen
-		if (data.i > length) {
-			return "";
-		}
-    */
+      bits = 0;
+      maxpower = mpow(2,numBits);
+      power=1;
+      while (power!=maxpower) {
+        resb = data.v & data.p;
+        data.p >>= 1;
+        if (data.p == 0) {
+          data.p = resetValue;
+          data.v = getNextValue(data.i++);
+        }
+        bits |= (resb>0 ? 1 : 0) * power;
+        power <<= 1;
+      }
 
-		bits = 0;
-		maxpower = mpow(2,numBits);
-		power=1;
-		while (power!=maxpower) {
-			resb = data.v & data.p;
-			data.p >>= 1;
-			if (data.p == 0) {
-				data.p = resetValue;
-				data.v = getNextValue(data.i++);
-			}
-			bits |= (resb>0 ? 1 : 0) * power;
-			power <<= 1;
-		}
+      switch (c = bits) {
+        case 0:
+          bits = 0;
+          maxpower = mpow(2,8);
+          power=1;
+          while (power!=maxpower) {
+            resb = data.v & data.p;
+            data.p >>= 1;
+            if (data.p == 0) {
+              data.p = resetValue;
+              data.v = getNextValue(data.i++);
+            }
+            bits |= (resb>0 ? 1 : 0) * power;
+            power <<= 1;
+          }
 
-		switch (c = bits) {
-			case 0:
-				bits = 0;
-				maxpower = mpow(2,8);
-				power=1;
-				while (power!=maxpower) {
-					resb = data.v & data.p;
-					data.p >>= 1;
-					if (data.p == 0) {
-						data.p = resetValue;
-						data.v = getNextValue(data.i++);
-					}
-					bits |= (resb>0 ? 1 : 0) * power;
-					power <<= 1;
-				}
+          dictionary[dictSize++] = f(bits);
+          c = dictSize-1;
+          enlargeIn--;
+          break;
+        case 1:
+          bits = 0;
+          maxpower = mpow(2,16);
+          power=1;
+          while (power!=maxpower) {
+            resb = data.v & data.p;
+            data.p >>= 1;
+            if (data.p == 0) {
+              data.p = resetValue;
+              data.v = getNextValue(data.i++);
+            }
+            bits |= (resb>0 ? 1 : 0) * power;
+            power <<= 1;
+          }
+          dictionary[dictSize++] = f(bits);
+          c = dictSize-1;
+          enlargeIn--;
+          break;
+        case 2:
+          return doc.write(result.join(''));
+      }
 
-				dictionary[dictSize++] = f(bits);
-				c = dictSize-1;
-				enlargeIn--;
-				break;
-			case 1:
-				bits = 0;
-				maxpower = mpow(2,16);
-				power=1;
-				while (power!=maxpower) {
-					resb = data.v & data.p;
-					data.p >>= 1;
-					if (data.p == 0) {
-						data.p = resetValue;
-						data.v = getNextValue(data.i++);
-					}
-					bits |= (resb>0 ? 1 : 0) * power;
-					power <<= 1;
-				}
-				dictionary[dictSize++] = f(bits);
-				c = dictSize-1;
-				enlargeIn--;
-				break;
-			case 2:
-        /* when done, generate a document with the result. */
-				doc.write(result.join(''));
-        return;
-		}
+      if (enlargeIn == 0) {
+        enlargeIn = mpow(2, numBits);
+        numBits++;
+      }
 
-		if (enlargeIn == 0) {
-			enlargeIn = mpow(2, numBits);
-			numBits++;
-		}
+      if (dictionary[c]) {
+        entry = dictionary[c];
+      } else {
+        if (c === dictSize) {
+          entry = w + w.charAt(0);
+        }
+       /* Should never happen
+        else {
+          return null;
+        }
+       */
+      }
+      result.push(entry);
 
-		if (dictionary[c]) {
-			entry = dictionary[c];
-		} else {
-			if (c === dictSize) {
-				entry = w + w.charAt(0);
-			} /* Should not happen else {
-       
-				return null;
-			} */
-		}
-		result.push(entry);
+      // Add w+entry[0] to the dictionary.
+      dictionary[dictSize++] = w + entry.charAt(0);
+      enlargeIn--;
 
-		// Add w+entry[0] to the dictionary.
-		dictionary[dictSize++] = w + entry.charAt(0);
-		enlargeIn--;
+      w = entry;
 
-		w = entry;
+      if (enlargeIn == 0) {
+        enlargeIn = mpow(2, numBits);
+        numBits++;
+      }
 
-		if (enlargeIn == 0) {
-			enlargeIn = mpow(2, numBits);
-			numBits++;
-		}
+    }
+})("BYUwNmD2Q===");
 
-	}
-
-})('');
